fe8bc7ce35df69ddf11efdeef47b46bdb28a1eea
Cengiz AKCAN <thevobos@gmail.com>
2020-12-13 14:46:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Create React App + Go API DDDD</h1>
       <h2>
         Deployed with{' '}
         <a
